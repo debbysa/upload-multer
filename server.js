077e2df3b0c21fdebfb2bfc60843939379f10bc3
debbysa <deby.silvia23@gmail.com>
2019-10-17 06:20:12 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,55 @@
+// call all the required packages
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const multer = require("multer");
+const path = require("path");
+//CREATE EXPRESS APP
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+// SET STORAGE
+var storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, "public/uploads");
+  },
+  filename: function(req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  }
+});
+
+var upload = multer({ storage: storage });
+
+app.post("/uploadfile", upload.single("myFile"), (req, res, next) => {
+  const file = req.file;
+  if (!file) {
+    const error = new Error("Please upload a file");
+    error.httpStatusCode = 400;
+    return next(error);
+  }
+  console.log(file.filename);
+  res.contentType("image/jpeg");
+  res.send(req.file.destination + "/" + req.file.filename);
+});
+
+// Uploading multiple files
+app.post("/uploadmultiple", upload.array("myFiles", 12), (req, res, next) => {
+  const files = req.files;
+  if (!files) {
+    const error = new Error("Please choose files");
+    error.httpStatusCode = 400;
+    return next(error);
+  }
+
+  res.send(files);
+});
+
+const port = 3000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));

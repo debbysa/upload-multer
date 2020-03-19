193d87dbd4cc56d5ca11b957381d86f5c9c57821
debbysa <deby.silvia23@gmail.com>
2020-03-19 23:55:28 +0700

    feat(doc): add note in code

--- src/api/note/noteController.js
+++ src/api/note/noteController.js
@@ -11,9 +11,11 @@ module.exports = {
     Note.create({
       judul: req.body.judul,
       isi: req.body.isi,
+      // mengirim path ke field di Database
       gambar: req.files === undefined ? "" : req.files[0].path,
       foto: req.files === undefined ? "" : req.files[1].path
     }).then(newNote => {
+      //mengirim kembali json
       res.json({
         status: "success",
         message: "Note added",
